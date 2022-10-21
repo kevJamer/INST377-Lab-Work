274f0fd6a91e30ba04904e88fe87cc71b02b678a
kevJamer <102767072+kevJamer@users.noreply.github.com>
2022-10-21 17:04:40 -0400

    processResturants works

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -5,11 +5,11 @@
   by adding `<script src="script.js">` just before your closing `</body>` tag
 */
 
-/*
-  ## Utility Functions
-    Under this comment place any utility functions you need - like an inclusive random number selector
-    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-*/
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
 
 function injectHTML(list) {
   console.log('fired injectHTML');
@@ -31,7 +31,12 @@ function injectHTML(list) {
 
 function processRestaurants(list) {
   console.log('fired restaurants list');
-
+  const range = [...Array(15).keys()];
+  const newArray = range.map((item) => {
+    const index = getRandomIntInclusive(0, list.length);
+    return list[index];
+  })
+  return newArray;
   /*
     ## Process Data Separately From Injecting It
       This function should accept your 1,000 records
@@ -106,7 +111,7 @@ async function mainEvent() {
 
       // This constant will have the value of your 15-restaurant collection when it processes
       const restaurantList = processRestaurants(arrayFromJson.data);
-
+      console.log(restaurantList)
       // And this function call will perform the "side effect" of injecting the HTML list for you
       injectHTML(restaurantList);
 
