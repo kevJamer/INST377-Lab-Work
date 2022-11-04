8115ec18a16447d2fa01e459b444f37cc369d1a4
kevJamer <102767072+kevJamer@users.noreply.github.com>
2022-11-04 16:48:06 -0400

    stuff

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -69,6 +69,17 @@ function getRandomIntInclusive(min, max) {
     */
   }
   
+  function filterList(list,filterInputValue) {
+    return list.filter((item) => {
+        if(!item.name) {return; }
+        const lowerCaseName = itme.name.toLowerCase();
+        const lowerCaseQuery = filterInputValue.toLowerCase();
+        return lowerCaseName.includes(lowerCaseQuery); 
+    });
+  }
+
+
+
   async function mainEvent() {
     /*
       ## Main Event
@@ -114,18 +125,22 @@ function getRandomIntInclusive(min, max) {
       loadAnimation.classList.remove('lds-ellipsis');
       loadAnimation.classList.add('lds-ellipsis_hidden');
     
-  
+      let currentList = [];
+
+      form.addEventListener('input', (event) => {
+        console.log(event.target.value);
+        const filteredList = filterList(currentList, event.target.value);
+        injectHTML(filteredList);
+      });
+
       // And here's an eventListener! It's listening for a "submit" button specifically being clicked
       // this is a synchronous event event, because we already did our async request above, and waited for it to resolve
       form.addEventListener('submit', (submitEvent) => {
         // This is needed to stop our page from changing to a new URL even though it heard a GET request
-        submitEvent.preventDefault();
-  
-        // This constant will have the value of your 15-restaurant collection when it processes
-        const restaurantList = processRestaurants(arrayFromJson.data);
-        console.log(restaurantList)
+        submitEvent.preventDefault(); 
+        currentList = processRestaurants(arrayFromJson.data);
         // And this function call will perform the "side effect" of injecting the HTML list for you
-        injectHTML(restaurantList);
+        injectHTML(currentList);
   
         // By separating the functions, we open the possibility of regenerating the list
         // without having to retrieve fresh data every time
